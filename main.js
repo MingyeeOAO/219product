a176efe0034fecb0f7f7ed9c3b2a8c7056469656
MingyeeOAO <lovelywmg@gmail.com>
2023-04-17 22:20:15 +0800

    update

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+
 window.addEventListener("scroll",function(){var nav = 
     document.getElementById("header-bar");
     nav.classList.toggle("active",window.scrollY > 0);
@@ -11,8 +13,25 @@ window.addEventListener("scroll",function(){var nav =
     function swch(){var swch= document.querySelector("body");
     swch.classList.toggle("swch");
 }
+
 var num =0;
 function get(){
     alert(num);
     num ++;
 }
+
+/*const appearOption = {}
+const faders = document.querySelectorAll('.img-box')
+const AppearOnScroll = new IntersectionObserver(function (entries, AppearOnScroll){
+    entries.forEach(entry =>{
+        if(!entry.isIntersecting){ return;}
+        else{
+            entry.target.classList.add('.appear');
+            AppearOnScroll.unobserve(entry.target);
+        }
+    });
+}, appearOption);
+
+faders.forEach(fader =>{
+    AppearOnScroll.observe(fader);
+})*/
