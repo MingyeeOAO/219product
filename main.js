e27b9091cf942047f065e1b8a1df96b49678a743
MingyeeOAO <lovelywmg@gmail.com>
2023-04-17 19:57:16 +0800

    reupload

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-
+window.addEventListener("scroll",function(){var nav = 
+    document.getElementById("header-bar");
+    nav.classList.toggle("active",window.scrollY > 0);
+    })
+    function back_home(){
+    document.getElementById("info").style.display="none";}  
+    function on(){
+    document.getElementById("slider").style.display="flex";}
+    function off(){
+    document.getElementById("slider").style.display="none";}
+    function swch(){var swch= document.querySelector("body");
+    swch.classList.toggle("swch");
+}
 var num =0;
 function get(){
     alert(num);
