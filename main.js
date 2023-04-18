8b11ab7f08c387262a7d8ba394edb0cd3444a13b
MingyeeOAO <lovelywmg@gmail.com>
2023-04-19 00:11:57 +0800

    update

--- main.js
+++ main.js
@@ -1,22 +1,33 @@
-
+function back_home(){
+    document.getElementById("info").style.display="none";
+}  
+function on(){
+    document.getElementById("slider").style.display="flex";
+}
+function off(){
+    document.getElementById("slider").style.display="none";
+}
+function swch(){var swch= document.querySelector("body");
+    swch.classList.toggle("swch");
+}
 window.addEventListener("scroll",function(){var nav = 
     document.getElementById("header-bar");
     nav.classList.toggle("active",window.scrollY > 0);
     })
-    function back_home(){
-    document.getElementById("info").style.display="none";}  
-    function on(){
-    document.getElementById("slider").style.display="flex";}
-    function off(){
-    document.getElementById("slider").style.display="none";}
-    function swch(){var swch= document.querySelector("body");
-    swch.classList.toggle("swch");
-}
+
 
 var num =0;
 function get(){
     alert(num);
     num ++;
+}
+
+function open(){
+    var popup = document.getElementById('popup');
+    //creatorpopup.style.display='flex';
+    popup.classList.add("open-popup")
+    //off();
+    
 }
 
 /*const appearOption = {}
