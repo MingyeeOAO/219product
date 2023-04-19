7152ce226a06f49dc18bb860826d82f97ad87b59
MingyeeOAO <lovelywmg@gmail.com>
2023-04-19 23:15:17 +0800

    big update!!

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var mode = true;
+
 function back_home(){
     document.getElementById("info").style.display="none";
 }  
@@ -9,6 +11,7 @@ function off(){
 }
 function swch(){var swch= document.querySelector("body");
     swch.classList.toggle("swch");
+    mode = !mode;
 }
 window.addEventListener("scroll",function(){var nav = 
     document.getElementById("header-bar");
@@ -21,15 +24,28 @@ function get(){
     alert(num);
     num ++;
 }
-
-function open(){
-    var popup = document.getElementById('popup');
-    //creatorpopup.style.display='flex';
-    popup.classList.add("open-popup")
-    //off();
-    
+function popup(){
+    var mes=document.getElementById('pop');
+    mes.style.transform="scale(1)";
+    mes.style.transitionTimingFunction="cubic-bezier(0,0,0,1.47)";
+    navigator.vibrate(250);
 }
 
+
+function popin(){
+    var mes=document.getElementById('pop');
+    mes.style.transform="scale(0)";
+    mes.style.transitionTimingFunction="cubic-bezier(0,0,0,-1.47)";
+}
+var options = {
+    host: 'private-github-api.com', // <-- Private github api url. If not passed, defaults to 'api.github.com'
+    pathPrefix: 'prefix-for-enterprise-instance', // <-- Private github api url prefix. If not passed, defaults to null.
+    protocol: 'https', // <-- http protocol 'https' or 'http'. If not passed, defaults to 'https'
+    user: 'Mingyee', // <-- Your Github username
+    repo: '119product', // <-- Your repository to be used a db
+    remoteFilename: 'data.json' // <- File with extension .json
+  };
+var personalAccessToken = "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";
 /*const appearOption = {}
 const faders = document.querySelectorAll('.img-box')
 const AppearOnScroll = new IntersectionObserver(function (entries, AppearOnScroll){
